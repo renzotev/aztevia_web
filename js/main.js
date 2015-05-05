463992f61eb476889dea34e2ff855eaa3e199004
renzotev <renzotelenta@gmail.com>
2015-05-04 19:55:01 -0500

    cambios

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
+$(function(){
+    $('.carousel').carousel({
+      interval: 5000
+    });
 
+    $('.percent').each( function () {
+    	var currNum = parseInt($(this).text());
+
+    	$(this).animateNumber(
+    		{ 
+	    		number: currNum
+	    	},
+	    	3000
+	    );
+    });
+
+    $window = $(window);
+ 
+   	$('.container[data-type="background"]').each(function(){
+     // declare the variable to affect the defined data-type
+     var $scroll = $(this);
+                     
+      $(window).scroll(function() {
+        // HTML5 proves useful for helping with creating JS functions!
+        // also, negative value because we're scrolling upwards                            
+        var yPos = -($window.scrollTop() / $scroll.data('speed'));
+         
+        // background position
+        var coords = '50% '+ yPos + 'px';
+ 
+        // move the background
+        $scroll.css({ backgroundPosition: coords });   
+      }); // end window scroll
+   	});
+});
