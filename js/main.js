7df0df6b8354b8d083c1844f57f4a1300bf6ed47
renzotev <renzotelenta@gmail.com>
2015-05-05 00:12:00 -0500

    primera fase

--- js/main.js
+++ js/main.js
@@ -13,4 +13,23 @@ $(function(){
 	    	3000
 	    );
     });
+
+    $window = $(window);
+ 
+   	$('.sports[data-type="background"]').each(function(){
+     // declare the variable to affect the defined data-type
+     var $scroll = $(this);
+                     
+      $(window).scroll(function() {
+        // HTML5 proves useful for helping with creating JS functions!
+        // also, negative value because we're scrolling upwards                            
+        var yPos = -($window.scrollTop() / $scroll.data('speed'));
+         
+        // background position
+        var coords = '50% '+ yPos + 'px';
+ 
+        // move the background
+        $scroll.css({ backgroundPosition: coords });   
+      }); // end window scroll
+   	});
 });
