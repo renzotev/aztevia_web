2f9e1c8cf5b284850325181855e9600fdb934852
renzotev <renzotelenta@gmail.com>
2015-05-29 07:52:25 -0500

    .

--- js/main.js
+++ js/main.js
@@ -14,4 +14,13 @@ $(function(){
 	    );
     });
 
+     /*$(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+            $(this).toggleClass('open');             
+        });*/
 });
